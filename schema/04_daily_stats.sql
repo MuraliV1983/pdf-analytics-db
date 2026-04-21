abfcf41cb8d0408cdc78a19f07acf2f0481640ff
MuraliV1983 <33620683+MuraliV1983@users.noreply.github.com>
2026-04-21 15:49:53 +0530

    Delete schema directory

--- schema/04_daily_stats.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pdf_daily_stats (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    pdf_id BIGINT UNSIGNED NOT NULL,
-    view_date DATE NOT NULL,
-    total_views INT DEFAULT 0,
-    unique_sessions INT DEFAULT 0,
-    UNIQUE KEY uk_pdf_date (pdf_id, view_date),
-    CONSTRAINT fk_daily_pdf FOREIGN KEY (pdf_id) REFERENCES pdfs(id)
-) ENGINE=InnoDB;
